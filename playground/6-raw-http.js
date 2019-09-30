8242e417d2ce882f5332e90133b218c08d8ff53c
Fabrizio Pace <pacefabriziowork@gmail.com>
2019-09-30 17:50:59 +0200

    Section 6 - Asynchronous Node.js (Weather App)

---
+++ playground/6-raw-http.js
@@ -0,0 +1,20 @@
+const https = require("https");
+const url = "https://api.darksky.net/forecast/1d28e78644f139cc63c23a3b0527d498/40,-75";;
+
+const request = https.request(url, (response) => {
+    let data = '';
+    response.on('data', (chunk) => {
+        data = data + chunk.toString();
+    });
+
+    response.on('end', () => {
+        const body = JSON.parse(data);
+        console.log(body);
+    });
+});
+
+request.on('error', (error) => {
+    console.log("An error", error);
+});
+
+request.end();
--- weather-app/app.js
+++ weather-app/app.js
@@ -1,8 +1,35 @@
-console.log("Starting");
+const geocode = require("./utils/geocode.js");
+const forecast = require("./utils/forecast.js");
+const yargs = require("yargs");
 
-setTimeout(() => {
-    console.log("2 Seconds Timer");
-}, 2000);
+yargs.command({
+    command: "geocode",
+    builder: {
+        city: {
+            describe: "City to check",
+            demandOption: true,
+            type: "string"
+        }
+    },
+    handler: (argv) => {
+        geocode(argv.city, (error, { latitude, longitude, location }) => {
+            if (error) {
+                return console.log(error);
+            };
 
+            forecast(latitude, longitude, (error, { summary, temperature, precipProbability }) => {
+                if (error) {
+                    return console.log(error);
+                }
 
-console.log("Stopping");
+                console.log(location);
+
+                const textCallback = summary + " It is currently " + temperature + " degress out. There is a " +
+                    precipProbability + "% chance of rain.";
+                console.log(textCallback);
+            });
+        });
+    }
+})
+
+yargs.parse();
